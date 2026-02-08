bc7cab419bdae72adacdd6895adb234d42518486
iosifdashkevich <iosifdashkevich@gmail.com>
2026-02-08 22:28:49 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,28 @@
 import asyncio
+import logging
+
 from aiogram import Bot, Dispatcher
-from aiogram.fsm.storage.memory import MemoryStorage
 
 from config import BOT_TOKEN
 from handlers import router
+from database import init_db
+
 
 async def main():
-    bot = Bot(BOT_TOKEN)
-    dp = Dispatcher(storage=MemoryStorage())
+    logging.basicConfig(level=logging.INFO)
+
+    # 🔥 создаём таблицу если её нет
+    init_db()
+
+    bot = Bot(token=BOT_TOKEN)
+    dp = Dispatcher()
+
     dp.include_router(router)
+
+    print("✅ Бот запущен")
+
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
